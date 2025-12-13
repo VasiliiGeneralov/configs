654bc979a39a58d389e701b15cb3958d092261a7
Vasilii Generalov <vasilii.generalov@yandex.ru>
2025-12-13 15:17:53 +0300

    Fix nvim lsp configuration
    Signed-off-by: Vasilii Generalov <vasilii.generalov@yandex.ru>

--- my_init_lua_extras.lua
+++ my_init_lua_extras.lua
@@ -1,8 +1,35 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    {"neovim/nvim-lspconfig"}
+  }
+})
+
 vim.o.scrolloff = 0
 
 vim.o.shiftwidth = 0
 vim.o.tabstop = 2
 vim.o.expandtab = true
+
+vim.o.mouse = ''
+
+vim.o.signcolumn = 'number'
 
 vim.cmd[[set termguicolors]]
 vim.cmd[[set background=dark]]
@@ -13,6 +40,7 @@ vim.cmd[[set completeopt+=menuone,noselect,popup]]
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp', {}),
   callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method('textDocument/completion') then
       local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       client.server_capabilities.completionProvider.triggerCharacters = chars
@@ -28,6 +56,9 @@ end, { expr = true })
 vim.keymap.set('i', '<S-Tab>', function()
   return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
 end, { expr = true })
+
+vim.keymap.set({'n'}, '<Leader>f', vim.lsp.buf.format)
+vim.keymap.set({'n'}, '<Leader>e', vim.diagnostic.open_float)
 
 vim.lsp.enable("clangd")
 vim.lsp.enable("pylsp")
